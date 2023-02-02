01e58bbceea7e0deace5ed7da3e620d8e3e97e43
Basil Njoga <Basilnjoga@gmail.com>
2023-02-02 21:58:52 +0300

    check

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotation to add two floating numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns sum of two floating numbers
+    """
+    return a + b
