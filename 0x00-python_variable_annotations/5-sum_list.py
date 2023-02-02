983ddc18b5e957e67d5d2208316971f651695d05
Basil Njoga <Basilnjoga@gmail.com>
2023-02-02 23:15:26 +0300

    check

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-annotating complex types and return sum
+annotating complex types and return sum of them
 """
 
 
 def sum_list(input_list: list[float]) -> float:
     """
-    takes a list of floats and returns sums
+    takes a list of floats and returns sum of them
     """
     sum = 0
     for i in range(len(input_list)):
