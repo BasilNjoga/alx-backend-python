0c91d80b22c153fbe6edea45e2d78c9324df1528
Basil Njoga <Basilnjoga@gmail.com>
2023-02-03 00:11:37 +0300

    duck typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+augmenting duck type annotations
+"""
+from typing import Any, Union
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Any) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
