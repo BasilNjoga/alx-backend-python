fa09f330d67d7a61ed64f1182feeddb2710a4945
Basil Njoga <Basilnjoga@gmail.com>
2023-02-02 23:19:33 +0300

    added typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,14 +2,14 @@
 """
 annotating complex types and return sum of them
 """
+from typing import List
 
-"""
-def sum_list(input_list: list[float]) -> float:
 
+def sum_list(input_list: List[float]) -> float:
+    """
     takes a list of floats and returns sum of them
-
+    """
     sum = 0
     for i in range(len(input_list)):
         sum = sum + input_list[i]
     return sum
-"""
