1e89773d50c12cb58482abdce22750f50eb1530a
Basil Njoga <Basilnjoga@gmail.com>
2023-02-03 00:14:06 +0300

    check

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-augmenting duck type annotations
+augmenting duck type annotations using various types
 """
 from typing import Any, Union
 
 
-def safe_first_element(lst: Any) -> Union[Any, None]:
+def safe_first_element(lst: Any) -> Union[Any]:
     """
-    returns an list of any type
+    returns an list of any type as given
     """
     if lst:
         return lst[0]
