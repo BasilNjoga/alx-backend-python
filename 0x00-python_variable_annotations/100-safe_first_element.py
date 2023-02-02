56b0f00e462dc5bdc6b2f970e1d4d98f0427cd51
Basil Njoga <Basilnjoga@gmail.com>
2023-02-03 00:12:34 +0300

    fixed

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,8 +4,12 @@ augmenting duck type annotations
 """
 from typing import Any, Union
 
+
 # The types of the elements of the input are not know
 def safe_first_element(lst: Any) -> Union[Any, None]:
+    """
+    returns an list of any type
+    """
     if lst:
         return lst[0]
     else:
